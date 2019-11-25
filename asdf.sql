6062e2dca83138d7a9631041f6faee13692a137e
Francisco Reinolds <francisco.reinolds@outlook.pt>
2019-11-25 00:05:14 +0000

    Collects tablespaces, datafiles and users. See description for more.
    To-Do:
    1. Collect Sessions, SGA, PGA and CPU.
    2. Further verify the creation script schema to see that it matches with our current needs.
    3. Alter the er_diagram.png and er_diagram.mwb
    4. Fix the README.md file to match the current tables.

--- asdf.sql
+++
@@ -1,8 +0,0 @@
-MERGE INTO bonuses dst
-USING ( SELECT '111' AS employee_id, '555' AS bonus FROM DUAL ) src
-ON ( dst.employee_id = src.employee_id )
-WHEN MATCHED THEN
-  UPDATE SET bonus = src.bonus
-WHEN NOT MATCHED THEN
-  INSERT ( employee_id, bonus )
-  VALUES ( src.employee_id, src.bonus );
